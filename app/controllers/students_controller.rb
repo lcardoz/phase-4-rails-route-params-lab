e5b990c501c786d456b84f4a7e65665d2ec43247
Leah Cardoz <leahcardoz@gmail.com>
2022-12-12 12:08:45 -0500

    commit tree f17b9f23bbda5370369663f547ff3543dcbe5174

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+
+      filtered_students = Student.all.filter do |student|
+        student.to_s.downcase.include?(params[:name])
+      end
+
+      render json: filtered_students
+      
+    else
+      students = Student.all
+      render json: students 
+    end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render json: @student
   end
 
 end
